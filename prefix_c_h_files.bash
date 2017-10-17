9bc6f261dd09d65edc258214d20205af17f702f3
Mahmoud Mohsen <mmohsen35@gmail.com>
2017-10-17 18:06:41 +0200

    Add prefix [gs] to headers & sources

--- prefix_c_h_files.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in $(find . -name '*.c'); do 
-	mv $file $(dirname $file)/gs_$(basename $file); 
-done
-
-for file in $(find . -name '*.h'); do 
-	mv $file $(dirname $file)/gs_$(basename $file); 
-done
